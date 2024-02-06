2e97d2d1c5dbdc4352c35f55a0d8bbef5cfe05e5
larsvv99 <lars.van.veldhuizen@hva.nl>
2024-02-06 14:59:13 +0100

    Html en css structuur aangepast
    Afbeeldingen toegevoegd
    
    Js en json file aangemaakt

---
+++ docs/js/script.js
@@ -0,0 +1,25 @@
+const teamMembers = {
+
+
+}
+
+teamMembers.members[0]
+
+const personalSiteInfo = {
+    "eend": {
+        "naam": "Lars",
+        "leeftijd": 168,
+        "eendenNaam": "Moquaak a.k.a. Redouan Kwakkie",
+        "karakter": "Baldadig",
+        "woonVijver": "Emiclaer vijver",
+        "favorieteBrood": "Kwarkbrood",
+        "kwikKwekKwak": "Kwik",
+        "favorieteDoelwit": [
+            {
+                "doelwit1": "Gordon",
+                "doelwit2": "Donald Duck",
+                "doelwit3": "Redouan Kwakkie"
+            }
+        ]
+    }
+}
